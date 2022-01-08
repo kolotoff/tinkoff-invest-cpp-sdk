e324be1c3ce5cbeeafdaf7becf5687ddfebfbb1d
Sergei Kolotovchenkov <s@kolotoff.com>
2022-01-08 07:42:49 +0300

    Добавлен комментарий

--- cmake/GetInvestApiProtosList.cmake
+++ cmake/GetInvestApiProtosList.cmake
@@ -2,6 +2,12 @@ set(InvestApiProtosDir "${CMAKE_SOURCE_DIR}/deps/investAPI/src/docs/contracts")
 set(InvestApiProtosOutDir "${CMAKE_SOURCE_DIR}/src/protos")
 file(GLOB InvestApiSrcProtos "${InvestApiProtosDir}/*.proto")
 
+# Proto файлы содержат в строке "package tinkoff.public.invest.api.*"
+# ключево словое С++ public, которое попадает в namespace.
+# Заменяем первую строчную буквку p на заглавную (public -> Public) для нормальной компиляции.
+# Для восстановления совместимости с сервером(обратной замены на строчную букву) 
+# используется RestoreProtosPackagePublicKeyword.cmake
+
 foreach (srcFilePath ${InvestApiSrcProtos})
     get_filename_component(filename ${srcFilePath} NAME)
     message("fix namespace in file ${filename}")    
